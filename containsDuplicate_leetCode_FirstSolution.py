c2ea7d23218762fdea2fbcae906ff8b45b584f09
Ibrahim Y. Salama <122559623+Hema4k@users.noreply.github.com>
2024-07-15 10:36:01 +0200

    Add files via upload

---
+++ containsDuplicate_leetCode_FirstSolution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        for i in range(len(nums)):
+            if(len(nums)<1 and len(nums)>10**5 or nums[i]<-10**9 or nums[i]>10**9):
+                return "sorry invalid input"
+            
+            for j in range(len(nums)):
+                if(i!=j and nums[i]== nums[j]):
+                    return True
+        return False
